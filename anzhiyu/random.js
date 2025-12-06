b055e15001db7a2822262b91db301e58a0794f29
666mingqing6 <666mingqing666@gmail.com>
2025-12-06 22:54:44 +0800

    Site updated: 2025-12-06 22:54:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025-12-05/lsp"];function toRandomPost(){
+var posts=["2025-12-05/lsp","2025-12-06/X"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
