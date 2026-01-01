d30a9e49923fbac33e1298d93d93370da11573a6
666mingqing6 <666mingqing666@gmail.com>
2026-01-01 15:37:38 +0800

    Site updated: 2026-01-01 15:37:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025-12-19/ksutomagisk/","2025-12-05/lsp/","2025-12-06/X/"];function toRandomPost(){
+var posts=["2026-01-01/experiencein2025/","2025-12-19/ksutomagisk/","2025-12-06/X/","2025-12-05/lsp/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
