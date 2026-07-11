1b08a229f167017e968487b59248e3373773b41f
666mingqing6 <666mingqing666@gmail.com>
2026-07-12 02:08:26 +0800

    Site updated: 2026-07-12 02:08:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,4 +1,4 @@
-var posts=["talk-crush/","Farewell/","Talk-is-learning-useful/","Alienation-of-Learning/","talk-study/","Love-alienation/","9008/","howtousefb/","howtouseadb/","D-K-Effect/","experiencein2025/","ksutomagisk/","X/","lsp/"];function toRandomPost(){
+var posts=["talk-crush/","Farewell/","Talk-is-learning-useful/","Alienation-of-Learning/","talk-study/","Love-alienation/","howtousefb/","howtouseadb/","D-K-Effect/","experiencein2025/","ksutomagisk/","X/","lsp/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };var friend_link_list=[];
     var refreshNum = 1;
