53f4381181f616897ecc554ed61e0288ef3825f8
666mingqing6 <666mingqing666@gmail.com>
2026-08-08 06:47:21 +0800

    Site updated: 2026-08-08 06:47:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["talk-crush/","consciousness/","Farewell/","Talk-is-learning-useful/","Alienation-of-Learning/","talk-study/","Love-alienation/","howtousefb/","howtouseadb/","D-K-Effect/","experiencein2025/","ksutomagisk/","X/","lsp/"];function toRandomPost(){
+var posts=["talk-crush/","consciousness/","Farewell/","Talk-is-learning-useful/","Alienation-of-Learning/","talk-study/","Love-alienation/","gfw/","howtousefb/","howtouseadb/","D-K-Effect/","experiencein2025/","ksutomagisk/","X/","lsp/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
